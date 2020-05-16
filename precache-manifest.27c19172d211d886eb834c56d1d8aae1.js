20be16c17d8bf1a4b0638fe0b5c3b18da84a6b2f
Danyu You <dyou@proofpoint.com>
2020-05-16 09:31:24 -0700

    Updates

---
+++ precache-manifest.27c19172d211d886eb834c56d1d8aae1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517941a818bfac3f3e81c30e3413da2c",
+    "url": "/dinopedia/index.html"
+  },
+  {
+    "revision": "b4b838be067154a880b3",
+    "url": "/dinopedia/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "3c2968ef8b2eb466d66b",
+    "url": "/dinopedia/static/js/2.32bdae52.chunk.js"
+  },
+  {
+    "revision": "b4b838be067154a880b3",
+    "url": "/dinopedia/static/js/main.08b619a9.chunk.js"
+  },
+  {
+    "revision": "cf5d4404ae9d158d489c",
+    "url": "/dinopedia/static/js/runtime~main.82ba9daf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bdd26639006e8271ab0cc2b8c40df9b.js"
+  "/dinopedia/precache-manifest.27c19172d211d886eb834c56d1d8aae1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dinopedia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
