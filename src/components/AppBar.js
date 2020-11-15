233cbd91a471cf7ee411dc639c79f9e504cabfa4
danyu <danyuyou@gmail.com>
2020-11-14 21:17:44 -0800

    Fix eslint

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Header.module.css';
 
-export default function () {
+export default function AppBar() {
   return (
     <div className={styles.nav}>
       <div>Welcome to Eric & Ethan's DinoPedia</div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import styles from './Nav.module.css'
 import React from 'react';
 
-export default function () {
+export default function Nav() {
   return (
     <div className={styles.nav}></div>
   )
