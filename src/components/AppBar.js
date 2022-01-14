abd11b14652107fabaeb7116299f7cf03b8229a7
Danyu You <danyu.you@workday.com>
2022-01-14 09:59:22 -0800

    Changed title

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from "react";
+import styles from "./Header.module.css";
 
 export default function AppBar() {
   return (
     <div className={styles.nav}>
-      <div>WELCOME TO ERIC & ETHAN'S DINOPEDIA</div>
+      <div>WELCOME TO ERIC & ETHAN'S DINO World</div>
     </div>
-  )
-}  
+  );
+}
